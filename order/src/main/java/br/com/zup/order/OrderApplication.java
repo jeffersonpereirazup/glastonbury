53dfc3af449a72d0e4bf5be2fec38253c70d715b
Jefferson Alves Pereira <jefferson.pereira@zup.com.br>
2020-03-15 15:39:31 -0300

    adicionando exemplos de log com jaeger

--- order/src/main/java/br/com/zup/order/OrderApplication.java
+++ order/src/main/java/br/com/zup/order/OrderApplication.java
@@ -1,7 +1,10 @@
 package br.com.zup.order;
 
+import io.jaegertracing.Configuration;
+import io.jaegertracing.internal.JaegerTracer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OrderApplication {
